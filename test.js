6a36e8b1e04bed61cc5ecfa045532070feeb5d13
bhawana99 <31150622+bhawana99@users.noreply.github.com>
2018-03-13 12:57:52 +0530

    Update test.js

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ function onSignIn(googleUser)
 	var profile = googleUser.getBasicProfile();
 	$("g-signin2").css("display","none");
 	$("data").css("dispaly","block");
-	$("myImg").attr("src",profile.getImageUrl());
+	$("#myImg").attr("src",profile.getImageUrl());
 	$("#email").text(profile.getEmail());
 }
  function signOut() 
