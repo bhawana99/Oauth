388eddab52422db90b7e68b41c81fec15b9759e4
bhawana <bhawanasahu999@gmail.com>
2018-03-13 12:03:23 +0530

    API js file

---
+++ API.js
@@ -0,0 +1,19 @@
+
+
+function onSignIn(googleUser)
+{
+	var profile = googleUser.getBasicProfile();
+	document.getElementsByClassName("g-signin2").style.display = 'none';
+	document.getElementsByClassName("data").style.display = 'block';
+	document.console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail());
+}
+ function signOut() 
+ {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+ }
