5741fe9242403cc48c2889298fbb2ea9a29463ff
bhawana99 <31150622+bhawana99@users.noreply.github.com>
2018-03-13 12:57:28 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,12 +1,10 @@
 function onSignIn(googleUser)
 {
 	var profile = googleUser.getBasicProfile();
-	document.getElementsByClassName("g-signin2").style.display = 'none';
-	document.getElementsByClassName("data").style.display = 'block';
-	document.console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail());
+	$("g-signin2").css("display","none");
+	$("data").css("dispaly","block");
+	$("myImg").attr("src",profile.getImageUrl());
+	$("#email").text(profile.getEmail());
 }
  function signOut() 
  {
