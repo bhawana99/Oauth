a30c7d270bced455d26fcd18968d75b4bb43bb15
bhawana99 <31150622+bhawana99@users.noreply.github.com>
2018-03-14 21:12:40 +0530

    Update test.js

--- test.js
+++ test.js
@@ -10,6 +10,8 @@ function onSignIn(googleUser)
  {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
-      console.log('User signed out.');
+          console.log("User signed out.");
+	  $(".g-signin2").css("display","block");
+	  $(".data").css("dispaly","none");
     });
  }
