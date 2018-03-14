cec657cb81c2594761a9e019589dd553cf794905
bhawana99 <31150622+bhawana99@users.noreply.github.com>
2018-03-14 22:07:30 +0530

    Update test.js

--- test.js
+++ test.js
@@ -6,12 +6,11 @@ function onSignIn(googleUser)
 	$("#myImg").attr("src",profile.getImageUrl());
 	$("#email").text(profile.getEmail());
 }
- function signOut() 
- {
+function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
-          console.log("User signed out.");
-	  $(".g-signin2").css("display","block");
-	  $(".data").css("dispaly","none");
+      console.log("User signed out.");
+      $(".g-signin2").css("display" , "block");
+      $(".data").css("display" , "none");
     });
- }
+  }
