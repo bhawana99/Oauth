763fe0db22a0f592490e98a284b65aca77a8c8bf
bhawana99 <31150622+bhawana99@users.noreply.github.com>
2018-03-14 22:35:10 +0530

    Update test.js

--- test.js
+++ test.js
@@ -10,7 +10,7 @@ function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
       document.getElementById("logout").innerHTML = "User Successfully loggedOut";
-      $(".g-signin2").css("display" , "block");
+      $(".g-signin2").css("display" , "none");
       $("#data").css("display" , "none");
     });
   }
