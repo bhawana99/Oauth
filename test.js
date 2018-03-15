dc9fa16ff5bf8b6ac32890ff495768f18159cca8
bhawana99 <31150622+bhawana99@users.noreply.github.com>
2018-03-15 18:27:56 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 function onSignIn(googleUser){
 	var profile = googleUser.getBasicProfile();
-	$("#data").css('dispaly' , "block");
+	$("#data").css("visibility" , "visible");
 	$(".g-signin2").css("display" , "none");
 	$("#myImg").attr("src",profile.getImageUrl());
 	$("#email").text(profile.getEmail());
@@ -8,8 +8,8 @@ function onSignIn(googleUser){
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
-      document.getElementById("logout").innerHTML = "User Successfully loggedOut";
       $(".g-signin2").css("display" , "block");
-      $("#data").css("display" , "none");
-    });
+      $("#data").css("visibility" , "hidden");
+      $("#logout").css("visibility" , "visible");
+       });
   }
